40bd24501efac0521763ee22b5c12aa506345fd3
bikashkc00000-ops <bikashkc00000@gmail.com>
2025-10-03 11:43:02 +0000

    uploaded

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,30 @@
+console.log('hello its me bikash')
 
+// inputing two numbers
+const num1 = 8;
+const num2 = 10;
+
+//adding the two numbers
+const sum = num1 + num2;
+
+// displaying the result 
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+const prompt = require('prompt-sync')(); 
+console.log("starting")
+const name = prompt('Enter your name: ');
+console.log(`Hello, ${name}`);
+
+const number = parseInt(prompt("Enter a number: "));
+
+if (number > 0 ) {
+    console.log("The number is positive");
+}
+
+else if (number == 0) {
+    console.log("The number is zero");
+}
+
+else {
+    console.log("The number is negative");
+}
